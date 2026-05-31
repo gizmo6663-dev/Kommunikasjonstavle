c3f8775762500b7e5c37b34114aa0b81ad7b4f5e
gizmo6663-dev <gizmo6663@gmail.com>
2026-05-31 19:35:53 +0200

    Update p4a_hooks.py

--- p4a_hooks.py
+++ p4a_hooks.py
@@ -1,40 +1,35 @@
 """
 p4a_hooks.py – widget-støtte for Kommunikasjonstavle.
-Kopierer res/ og java/ til Gradle-prosjektet og patcher AndroidManifest.xml.
+Kopierer res/ og java/ til Gradle-prosjektets src/main/,
+og patcher AndroidManifest.xml med KtWidget-receiver.
 """
-
 import os
 import glob
 import shutil
 import logging
 
-# Støtter både gammelt (prebuild_apk) og nytt (before_gradle_build) hook-navn
-def prebuild_apk(build, *args, **kwargs):
-    _run_hook(build)
-
 def before_gradle_build(ctx):
-    # For p4a nyere versjoner – bygg et dummy-objekt med buildozer.root_dir
-    class Dummy:
-        pass
-    dummy = Dummy()
-    dummy.buildozer = type('obj', (object,), {'root_dir': os.getcwd()})()
-    _run_hook(dummy)
-
-def _run_hook(build):
-    root = build.buildozer.root_dir
+    """
+    Kalles av python-for-android rett før gradle-bygg.
+    ctx har attributten buildozer.root_dir.
+    """
+    root = ctx.buildozer.root_dir
     res_src = os.path.join(root, 'res')
     java_src = os.path.join(root, 'java')
-    
-    pattern = os.path.join(root, '.buildozer', 'android', 'platform',
-                           'build-*', 'dists', '*', 'src', 'main')
+
+    # Finn den dynamiske stien til src/main/ i Gradle-prosjektet
+    pattern = os.path.join(
+        root, '.buildozer', 'android', 'platform',
+        'build-*', 'dists', '*', 'src', 'main')
     matches = glob.glob(pattern)
+
     if not matches:
         logging.warning('p4a_hooks: fant ikke src/main/')
         return
 
     for main_dir in matches:
-        logging.info('p4a_hooks: patcher %s', main_dir)
-        
+        logging.info('p4a_hooks: behandler %s', main_dir)
+
         # 1. Kopier res/
         if os.path.exists(res_src):
             res_dst = os.path.join(main_dir, 'res')
@@ -47,28 +42,28 @@ def _run_hook(build):
                         shutil.rmtree(dst_f)
                     shutil.copytree(src_f, dst_f)
                     logging.info('p4a_hooks: kopierte res/%s', folder)
-        
-        # 2. Kopier java/
+
+        # 2. Kopier java/ til src/main/java/
         if os.path.exists(java_src):
             java_dst = os.path.join(main_dir, 'java')
             if os.path.exists(java_dst):
                 shutil.rmtree(java_dst)
             shutil.copytree(java_src, java_dst)
             logging.info('p4a_hooks: kopierte java/')
-        
+
         # 3. Patch AndroidManifest.xml
         manifest = os.path.join(main_dir, 'AndroidManifest.xml')
         if not os.path.exists(manifest):
             logging.warning('p4a_hooks: manifest ikke funnet')
             continue
-        
+
         with open(manifest, 'r', encoding='utf-8') as f:
             content = f.read()
-        
+
         if 'KtWidget' in content:
             logging.info('p4a_hooks: KtWidget allerede i manifest')
             continue
-        
+
         receiver_block = '''
         <receiver
             android:name="no.askapp.kommunikasjonstavle.KtWidget"
@@ -81,11 +76,22 @@ def _run_hook(build):
                 android:name="android.appwidget.provider"
                 android:resource="@xml/kt_widget_info" />
         </receiver>'''
-        
+
         if '</application>' in content:
-            content = content.replace('</application>', receiver_block + '\n    </application>')
+            content = content.replace(
+                '</application>',
+                receiver_block + '\n    </application>')
             with open(manifest, 'w', encoding='utf-8') as f:
                 f.write(content)
             logging.info('p4a_hooks: KtWidget lagt til i manifest')
         else:
             logging.error('p4a_hooks: </application> ikke funnet')
+
+# For bakoverkompatibilitet med eldre p4a-versjoner (dersom before_gradle_build ikke kalles)
+def prebuild_apk(build, *args, **kwargs):
+    class Dummy:
+        pass
+    ctx = Dummy()
+    ctx.buildozer = Dummy()
+    ctx.buildozer.root_dir = build.buildozer.root_dir
+    before_gradle_build(ctx)
